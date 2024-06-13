fb19b7a66b13f5c910142ef15a977bce3462891d
julio <julioccv13@hotmail.com>
2024-06-13 08:24:02 -0500

    update

---
+++ vanti/vanti linea/vanti_linea_billing.sql
@@ -0,0 +1,43 @@
+SELECT 
+ca.clase_documento AS tipo_documento
+, ca.num_doc_oficial AS factura
+, r.ent_vol_calculo AS consumo
+, SAFE_DIVIDE(sum(d.impor_variable), sum(d.ent_vol_calculo)) AS tarifa
+, r.impor_mon_transaccion AS valor_facturado
+, (r.tot_partida_de_factura_incluido - r.impor_mon_transaccion) AS otros_conceptos
+, r.tot_partida_de_factura_incluido AS total_factura
+, r.fec_inic_validez AS inicio_periodo
+, r.fec_fin_validez AS fin_periodo
+, r.fec_venci_neto AS fecha_emision
+, MAX(ca.fec_compensacion) AS fecha_pago
+, SUM(ca.imp_mon_local) AS aplicacion
+, CASE 
+    WHEN AVG(CAST(ca.sts_compensacion AS INT64)) = 9 THEN "pago total"
+    WHEN AVG(CAST(ca.sts_compensacion AS INT64)) > 0 THEN "pago parcial"
+    ELSE "no pago" 
+  END AS estado_pago
+, (r.impor_mon_transaccion - SUM(ca.imp_mon_local)) AS saldo_factura
+FROM `datalake-vanti.prd_del_bi.facturacion_r` r
+LEFT JOIN `datalake-vanti.prd_del_bi.facturacion_d` d ON r.num_doc_oficial = d.num_doc_oficial
+LEFT JOIN `datalake-vanti.prd_del_bi.fi_ca` ca ON r.num_doc_oficial = ca.num_doc_oficial
+
+WHERE r.num_doc_oficial IS NOT NULL
+AND r.num_doc_oficial != ''
+AND ca.num_doc_oficial NOT IN ('INV', 'NOT_NUM_OFICIAL')
+AND ca.doc_compensacion IS NOT NULL
+AND imp_mon_local > 100
+
+AND r.fec_contab_documento = '2021-04-28'  AND r.num_doc_oficial = 'F24I3269800'
+AND d.fec_contab_documento = '2021-04-28'
+AND ca.fec_contab_documento = '2021-04-28'
+GROUP BY 
+ca.clase_documento
+, ca.num_doc_oficial
+, r.ent_vol_calculo
+, r.impor_mon_transaccion
+, r.tot_partida_de_factura_incluido
+, fec_inic_validez
+, fec_fin_validez
+, fec_venci_neto
+--PARTITION BY
+--CLUSTER BY
---
+++ vanti/vanti linea/vanti_linea_sd.sql
@@ -0,0 +1,58 @@
+WITH VENCIMIENTOS AS (
+SELECT 
+  num_doc_oficial AS factura, 
+  venci_neto      AS vencimiento
+FROM (
+  SELECT
+    *,   
+    row_number() over(
+      partition by num_doc_oficial
+      order by clase_documento
+    ) as orden
+  FROM (
+    SELECT 
+      distinct 
+        num_doc_oficial, 
+        venci_neto,
+        CASE clase_documento
+          WHEN 'FA' THEN '001FA'
+          WHEN 'SD' THEN '001SD'
+          ELSE clase_documento
+        END AS clase_documento 
+    FROM `datalake-vanti.prd_del_bi.fi_ca` 
+    WHERE 
+      fec_contab_documento  BETWEEN '2023-12-01' AND '2023-12-31'
+    )
+  ))
+
+SELECT 
+ca.clase_documento AS tipo_documento
+, sd.nro_referencia AS factura
+, (sum(sd.valor_neto)+sd.impte_impuesto) AS valor_facturado
+, (sum(sd.valor_neto)+sd.impte_impuesto) AS total_factura
+, sd.fec_factura AS fecha_emision
+, v.vencimiento AS fecha_vencimiento
+, MAX(ca.fec_compensacion) AS fecha_pago
+, SUM(ca.imp_mon_local) AS aplicacion
+, CASE 
+    WHEN AVG(SAFE_CAST(ca.sts_compensacion AS INT64)) = 9 THEN "pago total"
+    WHEN AVG(SAFE_CAST(ca.sts_compensacion AS INT64)) > 0 THEN "pago parcial"
+    ELSE "no pago" 
+  END AS estado_pago
+, (sum(sd.valor_neto)+sd.impte_impuesto) - SUM(ca.imp_mon_local) AS saldo_factura
+FROM `datalake-vanti.prd_del_bi.facturacion_sd` sd
+INNER JOIN `datalake-vanti.GC.GC_USUARIOS_SD` usu ON sd.usu_modificacion = usu.cod_usuario_str
+INNER JOIN `datalake-vanti.GC.GC_MATERIALES_SD` mat ON SAFE_CAST(sd.material as INT) = mat.Material_int
+JOIN `datalake-vanti.prd_del_bi.fi_ca` ca ON sd.nro_referencia = ca.num_doc_oficial
+JOIN VENCIMIENTOS v ON ca.num_doc_oficial = v.factura
+
+WHERE sd.nro_referencia IS NOT NULL
+AND sd.nro_referencia != ''
+AND est_contabilidad = 'C'
+AND est_fac_pedido = 'C'
+AND sd.nro_referencia not like '%I%'
+AND ca.doc_compensacion IS NOT NULL
+
+AND sd.fec_factura BETWEEN '2023-12-01' AND '2023-12-31'  --AND sd.nro_referencia = 'C15S113530'
+AND ca.fec_contab_documento BETWEEN '2023-12-01' AND '2023-12-31'
+GROUP BY ca.clase_documento, sd.nro_referencia,sd.valor_neto, sd.impte_impuesto, sd.fec_factura, v.vencimiento
